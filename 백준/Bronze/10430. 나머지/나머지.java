44a0a7be84dddbf7a4995bd96a8e9edc0c2e471f
oEclipse <135106969+oEclipse@users.noreply.github.com>
2024-01-29 17:30:50 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.print(((A%C) * (B%C))%C);
+    }
+}
