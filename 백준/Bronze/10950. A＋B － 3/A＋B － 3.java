05a33153c2f5bc996c49ecf24aea07d13c0b530d
oEclipse <135106969+oEclipse@users.noreply.github.com>
2024-02-06 16:54:32 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -5,20 +5,17 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         int howmany = Integer.parseInt(bf.readLine());
-        int[] nums = new int[2];
-        int[] sums = new int[howmany];
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         for(int i=0; i<howmany; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-            nums[0] = Integer.parseInt(st.nextToken());
-            nums[1] = Integer.parseInt(st.nextToken());
-            sums[i] = nums[0]+nums[1];
+            st = new StringTokenizer(bf.readLine(), " ");
+            sb.append(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()));
+            sb.append('\n');
         }
 
-        for(int sum:sums) {
-            System.out.println(sum);
-        }
+        System.out.println(sb);
     }
 }
